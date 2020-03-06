8e2186650e1caac14bb4a016fbc11f50257b9399
Julien Duquesne <ju.duquesne@hotmail.fr>
2020-03-06 17:50:29 +0100

    build python package

---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+setuptools.setup(
+    name="minotoring",
+    version="0.0.0",
+    author="Datarmada",
+    author_email="contact@datarmada.com",
+    description="Easy to use ML and plug and play monitoring tool.",
+    url="https://github.com/borisghidaglia/minotoring",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        "flask = ^1.1.1",
+    ],
+    include_package_data=True,
+    exclude_package_data={
+        "": ["Dockerfile", "docker-compose.yml"],
+    }
+)
