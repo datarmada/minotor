5d6cfebf9be78c0d8c52d67e99040efb603eb364
Julien Duquesne <31037359+Dju97@users.noreply.github.com>
2020-03-10 14:56:20 +0100

    [Features metrics] save json with train data (#4)
    * write json with numpy or pandas data
    
    * write tests for generated json
    
    * dedupe block of code
    
    * clarify if statement
    
    * PR reviews
    
    * change file type check

---
+++ minotoring/data_writer.py
@@ -0,0 +1,69 @@
+import json
+from abc import abstractmethod, ABC
+from pathlib import Path
+from typing import Dict, List
+
+import numpy as np
+import pandas as pd
+
+from minotoring.minotoring.constants import DATA_DIR
+
+
+class DataWriterABC(ABC):
+    def __init__(self, project_name: str):
+        project_dir = DATA_DIR / project_name
+        project_dir.mkdir(exist_ok=True, parents=True)
+        self.json_path: Path = project_dir / "data.json"
+        self.json_data: Dict = {}
+        if self.json_path.exists():
+            self.load_json()
+        else:
+            self.create_json()
+
+    def load_json(self):
+        with self.json_path.open('r') as f:
+            self.json_data = json.load(f)
+
+    def create_json(self):
+        self.json_data = {
+            "features": {}
+        }
+
+    def write_json_data(self):
+        with self.json_path.open('w') as f:
+            json.dump(self.json_data, f)
+
+    def fill_feature(self, feature_name: str, data: List, data_type: str = "Numeric"):
+        if feature_name not in self.json_data["features"]:
+            self.json_data["features"][feature_name] = {
+                "type": data_type,
+                "value_train": [],
+            }
+        self.json_data["features"][feature_name]["value_train"] = data
+
+    @abstractmethod
+    def fill_w_train(self, data) -> Dict:
+        pass
+
+
+class DataWriterNumpyArray(DataWriterABC):
+    def fill_w_train(self, data: np.ndarray) -> Dict:
+        assert type(data) == np.ndarray
+        data = data.transpose()
+        for i, feature_data in enumerate(data):
+            self.fill_feature(f"feature_{i}", feature_data.tolist(), "Numeric")
+        return self.json_data
+
+
+class DataWriterPandasDataframe(DataWriterABC):
+    def fill_w_train(self, data: pd.DataFrame) -> Dict:
+        assert type(data) == pd.DataFrame
+        for col in data:
+            self.fill_feature(col, data[col].values.tolist(), data[col].dtype)
+        return self.json_data
+
+
+type2data_writer = {
+    np.ndarray: DataWriterNumpyArray,
+    pd.DataFrame: DataWriterPandasDataframe
+}
--- minotoring/decorators.py
+++ minotoring/decorators.py
@@ -1,18 +1,27 @@
 import functools
-from typing import Callable, Tuple, Union, List
+from typing import Callable, Union
 
 import numpy as np
+import pandas as pd
 import requests
 
+from minotoring.minotoring.data_writer import type2data_writer
 
-def monitor(func: Callable):
-    functools.wraps(func)
 
-    def wrapper_data(data: Union[np.ndarray, List], *args, **kwargs):
-        assert type(data) == np.ndarray or type(data) == list
-        if type(data) == np.ndarray:
-            data = data.tolist()
-        requests.post("http://0.0.0.0:5000/data", json={'data': data})
-        return func(data, *args, **kwargs)
+def monitor_train(project_name: str):
+    def decorator_monitor(func: Callable):
+        functools.wraps(func)
 
-    return wrapper_data
+        def wrapper_data(data: Union[np.ndarray, pd.DataFrame], *args, **kwargs):
+            assert type2data_writer.get(type(data)) is not None, \
+                f"Your data type must be among {type2data_writer.keys()} "
+            data_writer = type2data_writer[type(data)](project_name)
+            json_file = data_writer.fill_w_train(data)
+            data_writer.write_json_file()
+
+            requests.post("http://0.0.0.0:5000/data", json=json_file)
+            return func(data, *args, **kwargs)
+
+        return wrapper_data
+
+    return decorator_monitor
