8ed312b8d9e2588ac22147bf80793bede445baee
Boris Ghidaglia <borisghidaglia@gmail.com>
2020-03-13 19:10:39 +0100

    navbar v0 ok: added Roboto fonts and some svgs

--- minotoring/front/src/components/navbar/NavBar.js
+++ minotoring/front/src/components/navbar/NavBar.js
@@ -1,19 +1,34 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+
+// Styles
 import '../../sass/navbar/navbar.scss';
+
+// SVGs
+import { ReactComponent as Logo } from '../../img/logos/minotaur.svg';
+import { ReactComponent as Features } from '../../img/logos/features.svg';
+import { ReactComponent as Predictions } from '../../img/logos/prediction.svg';
 
 export default function NavBar(props) {
   return (
-    <div>
-      <h1>
-        <Link to="/">Minotor.ai</Link>
-      </h1>
-      <h2>
-        <Link to="/predictions">Predictions analytics</Link>
-      </h2>
-      <h2>
-        <Link to="/features">Features analytics</Link>
-      </h2>
-    </div>
+    <ul className="nav">
+      <li>
+        <h1>
+          <Link to="/">
+            <Logo className="logo" />
+          </Link>
+        </h1>
+      </li>
+      <li>
+        <Link to="/predictions">
+          <Predictions className="logo" />
+        </Link>
+      </li>
+      <li>
+        <Link to="/features">
+          <Features className="logo" />
+        </Link>
+      </li>
+    </ul>
   );
 }
