2ecd3c08412d9a9c4a93111aec25620b761add9b
Julien Duquesne <ju.duquesne@hotmail.fr>
2020-03-16 16:51:32 +0100

    add callback to useEffect

--- minotoring/front/src/pages/featuresAnalytics.js
+++ minotoring/front/src/pages/featuresAnalytics.js
@@ -24,7 +24,10 @@ export default function FeaturesAnalytics(props) {
     ws.onclose = function(e) {
       console.log('WebSocket closed');
     };
-  }, []);
+    return function cleanup() {
+      ws.close()
+    }
+  });
 
   return (
     <div>
