c28cab3774a58eb0d4a8ddcd2111499b181dfbd3
Boris Ghidaglia <borisghidaglia@gmail.com>
2020-03-18 12:55:16 +0100

    saving WIP

--- minotoring/front/src/components/react-vis/ReactVisComponent.js
+++ minotoring/front/src/components/react-vis/ReactVisComponent.js
@@ -1,37 +1,62 @@
 import React, { useState } from 'react';
-import {
-  Crosshair,
-  HorizontalGridLines,
-  VerticalGridLines,
-  XAxis,
-  XYPlot,
-  YAxis
-} from 'react-vis';
+import { Crosshair, HorizontalGridLines, VerticalGridLines, XAxis, XYPlot, YAxis } from 'react-vis';
 
 export default function ReactVisComponent({ children, ...props }) {
-  const { xTitle, yTitle, width, height, data } = props;
+  const { xTitle, yTitle, width, height, data, trainData, predictData } = props;
   const [crosshairValues, setCrosshairValues] = useState([]);
 
+  // If no data at all, return null component
+  if (!(data || (trainData && predictData))) {
+    return null;
+  }
+  // Constants
   const AXIS_STYLE = {
     title: {
       fontWeight: 900,
       fontSize: '16px',
-      fill: 'black'
-    }
+      fill: 'black',
+    },
   };
-  return (
-    <XYPlot
-      height={height}
-      width={width}
-      onMouseLeave={() => setCrosshairValues([])}
-    >
-      <VerticalGridLines />
-      <HorizontalGridLines />
+
+  // Conditionnal child creation: 2 cases, if data is set
+  // or if trainData && predictData are
+  const renderedChildren = [];
+  if (data) {
+    const child = (
       <children.type
         {...children.props}
-        data={data}
+        opacity={0.5}
+        data={data.map(({ x, y }) => ({ x, y: y + 1 }))}
         onNearestX={(value, { index }) => setCrosshairValues([value])}
       />
+    );
+    renderedChildren.push(child);
+  } else {
+    const trainChild = (
+      <children.type
+        {...children.props}
+        color="grey"
+        opacity={0.5}
+        data={trainData}
+        onNearestX={(value, { index }) => setCrosshairValues([value])}
+      />
+    );
+    const predictChild = (
+      <children.type
+        {...children.props}
+        opacity={0.5}
+        data={predictData}
+        onNearestX={(value, { index }) => setCrosshairValues([value])}
+      />
+    );
+    renderedChildren.push(trainChild, predictChild);
+  }
+
+  return (
+    <XYPlot height={height} width={width} onMouseLeave={() => setCrosshairValues([])}>
+      <VerticalGridLines />
+      <HorizontalGridLines />
+      {renderedChildren.map((child) => child)}
       <XAxis title={xTitle} style={AXIS_STYLE} />
       <YAxis title={yTitle} style={AXIS_STYLE} />
       <Crosshair values={crosshairValues} />
--- minotoring/front/src/pages/FeaturesAnalytics.js
+++ minotoring/front/src/pages/FeaturesAnalytics.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Table from '../components/base-elements/Table';
 import useFeatureData from '../utils/data-managers/FeatureDataManager';
 import buildTableProps from '../utils/data-managers/FeatureTableAdapter';
@@ -6,31 +6,46 @@ import buildTableProps from '../utils/data-managers/FeatureTableAdapter';
 import AreaPlot from '../components/react-vis/AreaPlot';
 import BarPlot from '../components/react-vis/BarPlot';
 
-const onTrClicked = e => {
-  const tr = e.currentTarget;
-  const selectedFeature = tr.firstChild.innerText;
-  // TODO: display graphs related to selected feature
-  console.log(`User has selected the feature : ${selectedFeature}`);
-};
-
 export default function FeaturesAnalytics(props) {
+  const [activeFeature, setActiveFeature] = useState(null);
+  const [areaPlotData, setAreaPlotData] = useState({});
   const featureData = useFeatureData();
 
   // Constants
-  const AREA_SERIES_PROPS = {
+  const REACT_VIS_PROPS = {
     xTitle: "I'm axis X",
     yTitle: "And I'm axis Y",
     width: 600,
     height: 400,
-    data: []
+  };
+
+  // Event functions
+  const onTrClicked = (e) => {
+    const tr = e.currentTarget;
+    const selectedFeature = tr.firstChild.innerText;
+
+    if (selectedFeature != activeFeature) {
+      const data = featureData[selectedFeature];
+      const histTrain = hist2reactVisData(data.train.hist);
+      const histPredict = hist2reactVisData(data.predict.hist);
+      setActiveFeature(selectedFeature);
+      setAreaPlotData({ trainData: histTrain, predictData: histPredict });
+    }
   };
 
   return (
     <div id="features-analytics">
       <h1 style={{ marginBottom: '30px' }}>Features Analytics</h1>
       <Table {...buildTableProps(featureData)} onTrClicked={onTrClicked} />
-      <AreaPlot {...AREA_SERIES_PROPS} />
-      <BarPlot {...AREA_SERIES_PROPS} />
+      <AreaPlot
+        trainData={areaPlotData.trainData}
+        predictData={areaPlotData.predictData}
+        {...REACT_VIS_PROPS}
+      />
+      <BarPlot {...REACT_VIS_PROPS} />
     </div>
   );
 }
+
+// Utils
+const hist2reactVisData = ([Y, X]) => Y.map((y, idx) => ({ x: X[idx], y }));
