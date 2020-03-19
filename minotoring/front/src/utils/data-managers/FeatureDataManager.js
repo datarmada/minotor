aa6fd6bf1c179f72329041bd2c6605034471be16
Boris Ghidaglia <borisghidaglia@gmail.com>
2020-03-19 11:06:26 +0100

    Showcasing outliers with a fake function

--- minotoring/front/src/utils/data-managers/FeatureDataManager.js
+++ minotoring/front/src/utils/data-managers/FeatureDataManager.js
@@ -32,10 +32,25 @@ export const buildAreaPlotProps = (uniqueFeatureData) => {
 };
 
 export const buildScatterPlotProps = (uniqueFeatureData) => {
-  const outliersData = values2reactVisData(uniqueFeatureData.predict.values);
-  return [{ data: outliersData, name: 'Outliers' }];
+  const scatterPlotData = values2reactVisData(uniqueFeatureData.predict.values);
+  const [outliers, regularPoints] = splitOutliers(scatterPlotData, uniqueFeatureData.predict.mean);
+  return [
+    { data: regularPoints, name: 'Regular Points' },
+    { data: outliers, name: 'Outliers', color: 'red' },
+  ];
 };
 
 // Utils
+const splitOutliers = (data, mean) =>
+  data.reduce(
+    ([outliers, regularPoints], { x, y }) =>
+      // TODO: of course this condition has no sense. This function has been
+      // been made only to show how it could be done based on a variable in uniqueFeatureData
+      // here, the mean
+      y < 0.8 * mean || y > 1.2 * mean
+        ? [[...outliers, { x, y }], regularPoints]
+        : [outliers, [...regularPoints, { x, y }]],
+    [[], []],
+  );
 const hist2reactVisData = ([Y, X]) => Y.map((y, idx) => ({ x: X[idx], y }));
 const values2reactVisData = (values) => values.map((value, idx) => ({ x: idx, y: value }));
