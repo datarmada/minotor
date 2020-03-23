f99fc3551a82644f98f12680c0240cc32e9b4001
Boris Ghidaglia <borisghidaglia@gmail.com>
2020-03-23 18:33:34 +0100

    some refacto in DropDown

--- minotoring/front/src/components/base-elements/Dropdown.js
+++ minotoring/front/src/components/base-elements/Dropdown.js
@@ -1,55 +1,62 @@
 import PropTypes, { string } from 'prop-types';
 import React, { useEffect, useRef, useState } from 'react';
 
+// Utils
+const orderOptions = (keys, query = '') => {
+  if (query === '') {
+    return keys.sort();
+  }
+  const [selected, others] = keys.reduce(
+    ([passed, fails], key) =>
+      key.startsWith(query)
+        ? [[...passed, key], fails]
+        : [passed, [...fails, key]],
+    [[], []]
+  );
+  return [...selected.sort(), ...others.sort()];
+};
+
+// Event functions
+const keyPressWrapper = options => e => {
+  if (e.key === 'Enter') {
+    console.log(`Selected option is: ${options[0]}`);
+  }
+};
+const pageClickWrapper = (node, setter) => e => {
+  // click outside of the node wraps the dropdown
+  if (node && !node.contains(e.target)) {
+    setter(false);
+  }
+};
+const queryChangeWrapper = (options, setter) => e => {
+  const newOptions = orderOptions(options, e.currentTarget.value);
+  setter(newOptions);
+};
+const unwrapWrapper = setter => e => {
+  setter(true);
+  e.stopPropagation();
+};
+
 export default function Dropdown(props) {
   // props
-  const { name, options } = props;
+  const { name, options: propOptions } = props;
   // refs
   const mainDiv = useRef(null);
-  const nameDiv = useRef(null);
-  const eltContainerUl = useRef(null);
   // states
   const [active, setActive] = useState(false);
-  const [searchValue, setSearchValue] = useState(null);
+  const [options, setOptions] = useState([]);
 
-  // Utils
-  const orderOptions = keys => {
-    if (!nameDiv.current) {
-      return keys.sort();
-    }
-    const [selected, others] = keys.reduce(
-      ([passed, fails], key) =>
-        key.startsWith(searchValue)
-          ? [[...passed, key], fails]
-          : [passed, [...fails, key]],
-      [[], []]
-    );
-    return [...selected.sort(), ...others.sort()];
-  };
-
-  // Event functions
-  const handleKeyPress = e => {
-    if (e.key === 'Enter') {
-      console.log(`selected option is: ${orderOptions(options)[0]}`);
-    }
-  };
-  const handlePageClick = e => {
-    // click outside of the elements wraps the dropdown
-    if (mainDiv.current && !mainDiv.current.contains(e.target)) {
-      setActive(false);
-    }
-  };
-  const handleSearchChange = e => {
-    setSearchValue(e.currentTarget.value);
-  };
-  const unwrap = e => {
-    setActive(true);
-    e.stopPropagation();
-  };
   // className builders
   const buildDropdownClasses = () => (active ? 'dropdown active' : 'dropdown');
 
+  // Event functions
+  const handleKeyPress = keyPressWrapper(options);
+  const handlePageClick = pageClickWrapper(mainDiv.currentTarget, setActive);
+  const handleQueryChange = queryChangeWrapper(options, setOptions);
+  const unwrap = unwrapWrapper(setActive);
+
   useEffect(() => {
+    setOptions(orderOptions(propOptions));
     // Event Listeners
     document.addEventListener('click', handlePageClick);
     return () => {
@@ -60,19 +67,20 @@ export default function Dropdown(props) {
   return (
     <div ref={mainDiv} className={buildDropdownClasses()}>
       <input
-        ref={nameDiv}
         className="dropdown-name"
         onClick={unwrap}
         onKeyPress={handleKeyPress}
-        onChange={handleSearchChange}
+        onChange={handleQueryChange}
         placeholder={name}
       />
-      <ul ref={eltContainerUl} className="dropdown-elt-container">
-        {orderOptions(options).map(key => (
-          <li key={key} className="dropdown-elt">
-            {key}
-          </li>
-        ))}
+      <ul className="dropdown-elt-container">
+        {options.map(key => {
+          return (
+            <li key={key} className="dropdown-elt">
+              {key}
+            </li>
+          );
+        })}
       </ul>
     </div>
   );
