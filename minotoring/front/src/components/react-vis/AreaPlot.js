e5d2d70d8ed2f6bd293831b20645fde7a712e99e
Julien Duquesne <ju.duquesne@hotmail.fr>
2020-03-25 10:40:44 +0100

    import react vis style in main.scss

--- minotoring/front/src/components/react-vis/AreaPlot.js
+++ minotoring/front/src/components/react-vis/AreaPlot.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { AreaSeries } from 'react-vis';
-import '../../../node_modules/react-vis/dist/style.css';
 import ReactVisComponent from './ReactVisComponent';
 
 export default function AreaPlot(props) {
