3e9d0ce79c05d0e9139ca8ce1a6d653a148c1afa
Boris Ghidaglia <borisghidaglia@gmail.com>
2020-03-31 18:10:18 +0200

    Ordering utils.js

--- minotoring/front/src/utils/utils.js
+++ minotoring/front/src/utils/utils.js
@@ -1,3 +1,10 @@
+export const getPhaseKey = isTraining =>
+  isTraining ? 'training' : 'prediction';
+
+// Transform Python Hist format into ReactVis data
+export const hist2reactVisData = ([Y, X]) =>
+  Y.map((y, idx) => ({ x: X[idx], y }));
+
 // Mapping (key, value) of an object with a function
 export const mapObjectItems = (obj, func) =>
   Object.entries(obj).map(([key, value]) => func(key, value));
@@ -21,13 +28,6 @@ export const partitionWithThresholds = (
     elem => elem.y <= upperThreshold && elem.y >= lowerThreshold
   );
 
-// Transform Python Hist format into ReactVis data
-export const hist2reactVisData = ([Y, X]) =>
-  Y.map((y, idx) => ({ x: X[idx], y }));
-
 // Transform values into ReactVis Data
 export const values2reactVisData = values =>
   values.map((value, idx) => ({ x: idx, y: value }));
-
-export const getPhaseKey = isTraining =>
-  isTraining ? 'training' : 'prediction';
