4a39acfa95b7f251c77fa67375b7bc15764d497f
Boris Ghidaglia <borisghidaglia@gmail.com>
2020-04-08 19:40:19 +0200

    adding margin and making assert output more readable

--- minotoring/api/training_data_handler.py
+++ minotoring/api/training_data_handler.py
@@ -3,19 +3,21 @@ from minotoring.decorators.feature_monitoring import monitor_training_features
 import pandas as pd
 import pickle
 
+
 @monitor_training_features
 def save_data(data):
-  pass
+    pass
+
 
 class TrainingDataHandler(BaseRouteHandler):
-  def post(self):
-    try:
-      data = pickle.loads(self.request.files["training_data"][0]["body"])
-    except Exception as e:
-      self.set_status(400, "You must send a pickled file")
-      return
-    try:
-      save_data(data)
-    except Exception as e:
-      self.set_status(400, str(e))
-      return
+    def post(self):
+        try:
+            data = pickle.loads(self.request.files["training_data"][0]["body"])
+        except Exception as e:
+            self.set_status(400, "You must send a pickled file")
+            return
+        try:
+            save_data(data)
+        except Exception as e:
+            self.set_status(400, str(e))
+        return
