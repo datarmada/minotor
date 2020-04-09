1e9034f3c0c3b9ec2fe2f8447c4fa94f6455eab1
Julien Duquesne <31037359+Dju97@users.noreply.github.com>
2020-04-09 14:58:36 +0200

    Import training data from UI (#71)
    Training data can now be imported through a button on the dashboard when no training data is detected
    
    Co-authored-by: Boris Ghidaglia <borisghidaglia@gmail.com>

--- minotoring/front/src/pages/FeaturesAnalytics.js
+++ minotoring/front/src/pages/FeaturesAnalytics.js
@@ -1,32 +1,43 @@
 import React, { useEffect, useState } from 'react';
 
+import { isEmpty } from 'lodash';
+
 // Components
-import FeatureAnalyzer from '../components/analyzers/FeatureAnalyzer';
-import InputsAnalytics from '../components/inputs-table/InputsAnalytics';
+import FeatureAnalyzer from '../components/feature-analytics/FeatureAnalyzer';
+import InputsAnalytics from '../components/feature-analytics/InputsAnalytics';
+import TrainingDataUploader from '../components/feature-analytics/TrainingDataUploader';
 
 // Data Managers
 import { buildGetFetcher } from '../utils/data-managers/DataFetcher';
 
 // Utils
-const dataSetter = async (response, setFeatureData) => {
+const dataSetter = async (response, setFeatureData, setHasTrainingData) => {
   const data = await response.json();
   setFeatureData(data);
+  setHasTrainingData(isEmpty(data) || !isEmpty(data.valuesInfos.training.ids));
 };
 
 export default function FeaturesAnalytics() {
   const [featureData, setFeatureData] = useState({});
   const [selectedInputs, setSelectedInputs] = useState(new Set());
+  const [hasTrainingData, setHasTrainingData] = useState(true);
 
   useEffect(() => {
     const { fetchData, abortController } = buildGetFetcher('data', dataSetter);
-    fetchData(setFeatureData);
+    fetchData(setFeatureData, setHasTrainingData);
     return () => {
       abortController.abort();
     };
-  }, []);
+  }, [hasTrainingData]);
+
   return (
     <div className="page">
       <h1>Features Analytics</h1>
+      {!hasTrainingData ? (
+        <TrainingDataUploader
+          updateTrainingData={el => setHasTrainingData(el)}
+        />
+      ) : null}
       <div className="card-container column">
         <div className="card-margin">
           <FeatureAnalyzer
